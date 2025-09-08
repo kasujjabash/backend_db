ef69754d2835b56a4867882064fd64cfbb8fb159
kasujjabash <kasujjabash80@gmail.com>
2025-09-08 13:03:44 +0300

    New routes

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ server.get("/quotes", (req, res) => {
   res.send("Here is a random quote!");
 });
 
+
+server.post("/quotes", (req, res) => {
+  res.send("Quote created!");
+});
+
+server.post("/authors", (req, res) => {
+  res.send("Author created!");
+});
+
 const PORT = 4001;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
